040efb1a4a9ef5f02b37ac345ee535f4c468f3bb
MikeTrizna <triznam@si.edu>
2014-11-10 16:27:00 -0500

    Moved dashboard data to GitHub
    Moved dashboard data to GitHub, so that I can update SI-based dashboard
    outside of SI network.

---
+++ Dashboard/all_data.js
@@ -0,0 +1,450 @@
+var last_update = 'Monday, 10 November, 2014';
+var totals_data = 
+{
+  "za": {
+    "pcr": {
+      "total": 24,
+      "unique": 24
+    },
+    "extraction": {
+      "total": 24,
+      "unique": 24
+    },
+    "fims": {
+      "total": 49,
+      "unique": 49
+    },
+    "assembled": {
+      "total": 0,
+      "unique": 0
+    },
+    "sequenced": {
+      "total": 0,
+      "unique": 0
+    }
+  },
+  "mx": {
+    "pcr": {
+      "total": 0,
+      "unique": 0
+    },
+    "extraction": {
+      "total": 0,
+      "unique": 0
+    },
+    "fims": {
+      "total": 814,
+      "unique": 814
+    },
+    "assembled": {
+      "total": 0,
+      "unique": 0
+    },
+    "sequenced": {
+      "total": 0,
+      "unique": 0
+    }
+  },
+  "ke": {
+    "pcr": {
+      "total": 227,
+      "unique": 180
+    },
+    "extraction": {
+      "total": 280,
+      "unique": 233
+    },
+    "fims": {
+      "total": 680,
+      "unique": 680
+    },
+    "assembled": {
+      "total": 0,
+      "unique": 0
+    },
+    "sequenced": {
+      "total": 434,
+      "unique": 170
+    }
+  },
+  "ng": {
+    "pcr": {
+      "total": 0,
+      "unique": 0
+    },
+    "extraction": {
+      "total": 0,
+      "unique": 0
+    },
+    "fims": {
+      "total": 0,
+      "unique": 0
+    },
+    "assembled": {
+      "total": 0,
+      "unique": 0
+    },
+    "sequenced": {
+      "total": 0,
+      "unique": 0
+    }
+  }
+};
+var timeline_data = 
+{
+  "ke": [
+    {
+      "content": "KWS_AAM_001 (50)",
+      "group": "0",
+      "id": "0",
+      "start": "2014-08-18"
+    },
+    {
+      "content": "KWS_AAM_002 (48)",
+      "group": "0",
+      "id": "1",
+      "start": "2014-08-19"
+    },
+    {
+      "content": "NMK_EFGJ_P002 (94)",
+      "group": "0",
+      "id": "2",
+      "start": "2014-08-13"
+    },
+    {
+      "content": "NMK_EFGJ_P003 (76)",
+      "group": "0",
+      "id": "3",
+      "start": "2014-08-13"
+    },
+    {
+      "content": "NMK_GK_001 (58)",
+      "group": "0",
+      "id": "4",
+      "start": "2014-11-05"
+    },
+    {
+      "content": "NMK_JSK_001 (157)",
+      "group": "0",
+      "id": "5",
+      "start": "2014-11-05"
+    },
+    {
+      "content": "NMK_JSK_002 (23)",
+      "group": "0",
+      "id": "6",
+      "start": "2014-11-10"
+    },
+    {
+      "content": "NMK_JSK_003 (23)",
+      "group": "0",
+      "id": "7",
+      "start": "2014-11-10"
+    },
+    {
+      "content": "NMK_JSK_004 (23)",
+      "group": "0",
+      "id": "8",
+      "start": "2014-11-10"
+    },
+    {
+      "content": "NMK_JSK_005 (23)",
+      "group": "0",
+      "id": "9",
+      "start": "2014-11-10"
+    },
+    {
+      "content": "NMK_JSK_006 (23)",
+      "group": "0",
+      "id": "10",
+      "start": "2014-11-10"
+    },
+    {
+      "content": "NMK_JSK_007 (19)",
+      "group": "0",
+      "id": "11",
+      "start": "2014-11-10"
+    },
+    {
+      "content": "NMK_PAA_001 (63)",
+      "group": "0",
+      "id": "12",
+      "start": "2014-08-21"
+    },
+    {
+      "content": "NMK_EWM_002_X01 (24)",
+      "group": "1",
+      "id": "13",
+      "start": "2014-07-28"
+    },
+    {
+      "content": "NMK_EWM_002_P001_rbcla-F_rbclr590 (24)",
+      "group": "2",
+      "id": "14",
+      "start": "2014-07-30"
+    },
+    {
+      "content": "NMK_EWM_002_X02 (70)",
+      "group": "1",
+      "id": "15",
+      "start": "2014-10-08"
+    },
+    {
+      "content": "NMK_EWM_003_X01 (48)",
+      "group": "1",
+      "id": "16",
+      "start": "2014-10-08"
+    },
+    {
+      "content": "NMK_EWM_003_X02 (26)",
+      "group": "1",
+      "id": "17",
+      "start": "2014-10-08"
+    },
+    {
+      "content": "NMK_EWM_002_P002_rbcla-F_rbclr590 (70)",
+      "group": "2",
+      "id": "18",
+      "start": "2014-08-25"
+    },
+    {
+      "content": "NMK_EWM_003_P001_rbcla-F_rbclr590 (48)",
+      "group": "2",
+      "id": "19",
+      "start": "2014-10-08"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "2",
+      "id": "20",
+      "start": "2014-10-21"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "21",
+      "start": "2014-10-09"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "22",
+      "start": "2014-10-09"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "23",
+      "start": "2014-10-09"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "24",
+      "start": "2014-10-22"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "25",
+      "start": "2014-10-09"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "26",
+      "start": "2014-10-09"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "27",
+      "start": "2014-10-09"
+    },
+    {
+      "content": "NMK_EWM_003_P002_rbcla-F_rbclr590 (26)",
+      "group": "3",
+      "id": "28",
+      "start": "2014-10-09"
+    },
+    {
+      "content": "NMK_EWM_002_iX01 (5)",
+      "group": "1",
+      "id": "29",
+      "start": "2014-06-18"
+    },
+    {
+      "content": "NMK_EWM_002_iP001_ITS2F_ITS3R (5)",
+      "group": "2",
+      "id": "30",
+      "start": "2014-06-14"
+    },
+    {
+      "content": "NMK_EWM_002_iP001_ITS2F_ITS3R (5)",
+      "group": "3",
+      "id": "31",
+      "start": "2014-10-10"
+    },
+    {
+      "content": "NMK_EWM_002_iP001_ITS2F_ITS3R (5)",
+      "group": "3",
+      "id": "32",
+      "start": "2014-10-10"
+    },
+    {
+      "content": "NMK_EWM_003_iX01 (10)",
+      "group": "1",
+      "id": "33",
+      "start": "2014-06-18"
+    },
+    {
+      "content": "NMK_EWM_003_iP001_ITS2F_ITS3R (10)",
+      "group": "2",
+      "id": "34",
+      "start": "2014-07-14"
+    },
+    {
+      "content": "NMK_EWM_003_iP001_ITS2F_ITS3R (10)",
+      "group": "3",
+      "id": "35",
+      "start": "2014-10-10"
+    },
+    {
+      "content": "NMK_EWM_003_iP001_ITS2F_ITS3R (10)",
+      "group": "3",
+      "id": "36",
+      "start": "2014-10-10"
+    },
+    {
+      "content": "NMK_EWM_002_iX02 (24)",
+      "group": "1",
+      "id": "37",
+      "start": "2014-10-22"
+    },
+    {
+      "content": "NMK_EWM_002_iP002_ITS2F_ITS3R (24)",
+      "group": "2",
+      "id": "38",
+      "start": "2014-07-14"
+    },
+    {
+      "content": "NMK_EWM_002_iP002_ITS2F_ITS3R (24)",
+      "group": "3",
+      "id": "39",
+      "start": "2014-10-13"
+    },
+    {
+      "content": "NMK_EWM_002_iP002_ITS2F_ITS3R (24)",
+      "group": "3",
+      "id": "40",
+      "start": "2014-10-13"
+    },
+    {
+      "content": "NMK_EWM_003_iX02 (10)",
+      "group": "1",
+      "id": "41",
+      "start": "2014-06-18"
+    },
+    {
+      "content": "NMK_EWM_003_iP002_ITS2F_ITS3R (10)",
+      "group": "2",
+      "id": "42",
+      "start": "2014-10-03"
+    },
+    {
+      "content": "NMK_EWM_003_iP002_ITS2F_ITS3R (10)",
+      "group": "3",
+      "id": "43",
+      "start": "2014-10-15"
+    },
+    {
+      "content": "NMK_EWM_003_iP002_ITS2F_ITS3R (10)",
+      "group": "3",
+      "id": "44",
+      "start": "2014-10-15"
+    },
+    {
+      "content": "NMK_RMM_001_X01 (63)",
+      "group": "1",
+      "id": "45",
+      "start": "2014-07-02"
+    },
+    {
+      "content": "NMK_RMM_001_P001 (10)",
+      "group": "2",
+      "id": "46",
+      "start": "2014-07-14"
+    }
+  ],
+  "mx": [
+    {
+      "content": "IBUNAM_AJM_001 (96)",
+      "group": "0",
+      "id": "0",
+      "start": "2014-10-14"
+    },
+    {
+      "content": "IBUNAM_AJM_002 (96)",
+      "group": "0",
+      "id": "1",
+      "start": "2014-10-27"
+    },
+    {
+      "content": "IBUNAM_AJM_003 (146)",
+      "group": "0",
+      "id": "2",
+      "start": "2014-10-27"
+    },
+    {
+      "content": "IBUNAM_LCM_001 (95)",
+      "group": "0",
+      "id": "3",
+      "start": "2014-08-26"
+    },
+    {
+      "content": "IBUNAM_LCM_002 (95)",
+      "group": "0",
+      "id": "4",
+      "start": "2014-09-03"
+    },
+    {
+      "content": "IBUNAM_LCM_003 (96)",
+      "group": "0",
+      "id": "5",
+      "start": "2014-09-08"
+    },
+    {
+      "content": "IBUNAM_LCM_004 (95)",
+      "group": "0",
+      "id": "6",
+      "start": "2014-09-26"
+    },
+    {
+      "content": "IBUNAM_LICM_001 (95)",
+      "group": "0",
+      "id": "7",
+      "start": "2014-11-07"
+    }
+  ],
+  "ng": [],
+  "za": [
+    {
+      "content": "ACDB_ZES_001 (49)",
+      "group": "0",
+      "id": "0",
+      "start": "2014-10-13"
+    },
+    {
+      "content": "NZG_MR_TR01_COI (24)",
+      "group": "2",
+      "id": "1",
+      "start": "2014-11-10"
+    },
+    {
+      "content": "NZG_MR_TR01_X01 (24)",
+      "group": "1",
+      "id": "2",
+      "start": "2014-11-10"
+    }
+  ]
+};
