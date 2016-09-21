f23c9a98dd71784e0e26c4c6b84709a75156a1c9
Mike Trizna <triznam@si.edu>
2016-09-21 18:28:28 -0400

    first dashboard update in a long long time
    Due to database issues and updating the script for Python3
    compatibility.

--- Dashboard/all_data.js
+++ Dashboard/all_data.js
@@ -1,76 +1,76 @@
-var last_update = 'Monday, 27 June, 2016 at 14:21 EST';
+var last_update = 'Wednesday, 21 September, 2016 at 18:18 EST';
 var totals_data = {
   "ke": {
     "fims": {
-      "total": 815, 
+      "total": 815,
       "unique": 815
-    }, 
+    },
     "extraction": {
-      "total": 978, 
+      "total": 978,
       "unique": 783
-    }, 
+    },
     "pcr": {
-      "total": 2427, 
-      "unique": 746
-    }, 
+      "total": 2464,
+      "unique": 761
+    },
     "sequenced": {
-      "total": 3484, 
-      "unique": 732
-    }, 
+      "total": 3642,
+      "unique": 752
+    },
     "passed": {
-      "total": 1797, 
-      "unique": 519
-    }, 
+      "total": 1949,
+      "unique": 583
+    },
     "gb": {
       "total": 0
     }
-  }, 
+  },
   "mx": {
     "fims": {
-      "total": 1837, 
+      "total": 1837,
       "unique": 1837
-    }, 
+    },
     "extraction": {
-      "total": 1650, 
+      "total": 1650,
       "unique": 1523
-    }, 
+    },
     "pcr": {
-      "total": 6317, 
+      "total": 6618,
       "unique": 1521
-    }, 
+    },
     "sequenced": {
-      "total": 5281, 
+      "total": 5281,
       "unique": 1275
-    }, 
+    },
     "passed": {
-      "total": 4053, 
+      "total": 4053,
       "unique": 844
-    }, 
+    },
     "gb": {
       "total": 0
     }
-  }, 
+  },
   "za": {
     "fims": {
-      "total": 980, 
+      "total": 980,
       "unique": 980
-    }, 
+    },
     "extraction": {
-      "total": 1002, 
+      "total": 1002,
       "unique": 1000
-    }, 
+    },
     "pcr": {
-      "total": 2324, 
+      "total": 2327,
       "unique": 967
-    }, 
+    },
     "sequenced": {
-      "total": 3730, 
+      "total": 3731,
       "unique": 883
-    }, 
+    },
     "passed": {
-      "total": 2515, 
+      "total": 2516,
       "unique": 695
-    }, 
+    },
     "gb": {
       "total": 0
     }
